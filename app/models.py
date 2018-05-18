d0484a2235745568650eb3b9e766b6ac2cb65d03
VickyMutai <vicky.mutai96@gmail.com>
2018-05-18 12:10:34 +0300

    creating models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length =30)
+
+class Chapters(models.Model):
+    chapter_name = models.CharField(max_length=60)
+    subject = models.ForeignKey(Subject)
+
+class Topics(models.Model):
+    topic_name = models.CharField(max_length=60)
+    chapter = models.ForeignKey(Chapters)
+    subject = models.ForeignKey(Subject)
