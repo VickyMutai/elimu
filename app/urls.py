1a361a4521bd443af817e4cfd7b37a400df5de42
VickyMutai <vicky.mutai96@gmail.com>
2018-05-18 11:52:45 +0300

    setting up logout

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-python from django.contrib.auth import views
 
 urlpatterns=[
      url('^$',views.index,name = 'index'),
-     url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
